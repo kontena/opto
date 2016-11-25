c7a0984847d01d6a5655f41969df53509c6ccde1
Kimmo Lehto <kimmo.lehto@kontena.io>
2016-11-25 10:35:10 +0200

    Added the ignore_errors to file resolver mentioned in readme

--- lib/opto/resolvers/file_content.rb
+++ lib/opto/resolvers/file_content.rb
@@ -3,9 +3,21 @@ module Opto
     # Read the value from a file, path defined in hint.
     class File < Opto::Resolver
 
+      def ignore_errors?
+        return false unless hint.kind_of?(Hash) && (hint['ignore_errors'] || hint[:ignore_errors])
+      end
+
+      def file_path
+        hint.kind_of?(String) ? hint : (hint['path'] || hint[:path])
+      end
+
       def resolve
-        raise ArgumentError, "File path not set" unless hint.kind_of?(String)
-        ::File.read(hint)
+        raise ArgumentError, "File path not set" unless file_path
+        if ignore_errors?
+          ::File.read(hint) rescue nil
+        else
+          ::File.read(hint)
+        end
       end
     end
   end
