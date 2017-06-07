f1e11b5e86fcfd52f352c94d4b082db9da430da2
Daniel Battaglia <daniels.bytes@gmail.com>
2017-06-07 16:10:42 -0400

    Fix for github issue #32: 'File resolver ignore_errors option broken' (#33)

--- spec/opto/resolvers/file_content_spec.rb
+++ spec/opto/resolvers/file_content_spec.rb
@@ -13,6 +13,20 @@ describe Opto::Resolvers::File do
     it 'raises if path is not set' do
       expect{subject.new(nil).resolve}.to raise_error(ArgumentError)
     end
+
+    it 'raises if file does not exist' do
+      expect {
+        subject.new('/tmp/foo').resolve
+      }.to raise_error(Errno::ENOENT)
+    end
+
+    it 'returns nil if file does not exist and ignore_errors is set' do
+      hint = {
+        path: '/tmp/foo',
+        ignore_errors: true
+      }
+      expect(subject.new(hint).resolve).to be_nil
+    end
   end
 end
 
